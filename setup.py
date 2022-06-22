f8a8849065a782dc09deabeb489d8e81cafc998c
AlexAuragan <alexauragan@gmail.com>
2022-06-22 11:07:28 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='TAL-AI',
+      version='0.1',
+      description='Team Analysis for League of Legends - AI',
+      author='AlexAuragan, Theonakk',
+      author_email='alexauragan@gmail.com',
+      url='https://github.com/AlexAuragan/TAL-AI/',
+     )
