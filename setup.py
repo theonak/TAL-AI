24b99aab6b3e3b8390bdbc410c099aea52d62d6c
AlexAuragan <alexauragan@gmail.com>
2022-06-22 11:54:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,7 @@ from distutils.core import setup
 
 
 
-package_dir = {'':'talai',
-               'champs':'talai',
-               'data':'talai',
-               'models':'talai',
-               'utils':'talai',}
-
+py_modules  = ["champs","data","models","utils"]
 setup(name='talai',
       version='0.1',
       description='Team Analysis for League of Legends - AI',
@@ -15,5 +10,5 @@ setup(name='talai',
       author_email='alexauragan@gmail.com',
       url='https://github.com/AlexAuragan/TAL-AI/',
       packages=['talai'],
-      package_dir=package_dir 
+      py_modules=py_modules 
      )
