1da6d8e127ce6e19dc803856a76fa7be14dd93a9
AlexAuragan <alexauragan@gmail.com>
2022-06-22 11:49:23 +0200

    explicit module vs package

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from distutils.core import setup
 
+
+
+package_dir = {'':'talai',
+               'champs':'talai',
+               'data':'talai',
+               'models':'talai',
+               'utils':'talai',}
+
 setup(name='talai',
       version='0.1',
       description='Team Analysis for League of Legends - AI',
       author='AlexAuragan, Theonakk',
       author_email='alexauragan@gmail.com',
       url='https://github.com/AlexAuragan/TAL-AI/',
+      packages=['talai'],
+      package_dir=package_dir 
      )
